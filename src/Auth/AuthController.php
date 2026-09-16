6101d0d9e4bc14d266c65da06f1345a6d2da8765
jakeinman05 <jakeinman05@gmail.com>
2026-09-16 19:18:32 -0400

    Initialization of AuthController util class for controlling login endpoints.

---
+++ src/Auth/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Auth;
+
+class AuthController
+{
+    public function __construct() {}
+    public function registerUser(): void {}
+
+    public function loginUser(): void {}
+
+    public function logoutUser(): void {}
+}
