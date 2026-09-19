6874b949910739f8c6a1873ac577091db86b8e3f
jakeinman05 <jakeinman05@gmail.com>
2026-09-19 00:53:05 -0400

    Implemented registerUser() for properly creating accounts in user database

--- src/Auth/AuthController.php
+++ src/Auth/AuthController.php
@@ -2,10 +2,32 @@
 
 namespace App\Auth;
 
+use App\UserRepository;
+use JsonException;
+
 class AuthController
 {
-    public function __construct() {}
-    public function registerUser(): void {}
+    public function __construct(private readonly UserRepository $ur) {}
+
+    /**
+     * @throws JsonException
+     */
+    public function registerUser(): void {
+        $creds = APIAuthUtil::grabAndValidateCredentials();
+
+        if($creds === null) return;
+
+        $user = $creds["email"];
+        $pass = $creds["password"];
+
+        $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+
+        $user_id = $this->ur->create($user, $hashedPass);
+
+        APIAuthUtil::sendResponseCodeBody(201, [
+            "user_id" => $user_id
+        ]);
+    }
 
     public function loginUser(): void {}
 
